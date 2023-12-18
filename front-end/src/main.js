23b662b1f968ee1878f6c77e1aaedfc8013bc04a
Flopiras <Flopiras@yahoo.it>
2023-12-18 10:00:14 +0100

    import script of bootstrap

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 // bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
+//script of bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import App from './App.vue'
 
 createApp(App).mount('#app')
