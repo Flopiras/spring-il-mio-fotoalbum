35a08b330705ebc196cc31d1197168cf640b68ea
Flopiras <Flopiras@yahoo.it>
2023-12-18 17:30:21 +0100

    added detail page for message and delete method

--- src/main/java/org/java/spring/controller/MessageController.java
+++ src/main/java/org/java/spring/controller/MessageController.java
@@ -3,11 +3,14 @@ package org.java.spring.controller;
 import java.util.List;
 
 import org.java.spring.db.pojo.Message;
+import org.java.spring.db.pojo.Photo;
 import org.java.spring.db.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -25,6 +28,26 @@ public class MessageController {
 		model.addAttribute("messages", messages);
 		
 		return "messages";
+	}
+	
+	@GetMapping("/messages/{id}")
+	public String messageDetail(Model model, @PathVariable int id) {
+		
+		Message message = messageService.findById(id);
+		
+		model.addAttribute("message", message);
+		
+		return "message";
+	}
+	
+	@PostMapping("messages/delete/{id}")
+	public String deletePhoto(@PathVariable int id) {
+		
+		Message message = messageService.findById(id);
+		
+		messageService.delete(message);
+		
+		return "redirect:/messages";
 	}
 
 }
