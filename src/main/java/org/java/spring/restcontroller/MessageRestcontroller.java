b5457bbb8aa4d34dd7d0da93f7defc3da34ac958
Flopiras <Flopiras@yahoo.it>
2023-12-18 16:24:19 +0100

    added create for message

--- src/main/java/org/java/spring/restcontroller/MessageRestcontroller.java
+++ src/main/java/org/java/spring/restcontroller/MessageRestcontroller.java
@@ -3,12 +3,17 @@ package org.java.spring.restcontroller;
 import java.util.List;
 
 import org.java.spring.db.pojo.Message;
+import org.java.spring.db.pojo.Photo;
 import org.java.spring.db.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +32,14 @@ public class MessageRestcontroller {
 		
 		return new ResponseEntity<>(messages, HttpStatus.OK);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Message> create(@RequestBody Message message) {
+		
+		messageService.save(message);
+		
+		return new ResponseEntity<>(message, HttpStatus.OK);
+	}
+	
+	
 }
