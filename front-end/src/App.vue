d9a777100056ddd4a26c5626c2a451ae12c9a847
Flopiras <Flopiras@yahoo.it>
2023-12-18 10:29:12 +0100

    add header

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,33 @@
-<script setup>
+<script>
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
 
+const endpoint = "http://localhost:8080/api/photos";
+
+export default {
+  components: { AppHeader },
+
+  data() {
+    return {
+      photos: [],
+    }
+  },
+
+  methods: {
+    async getPhotos(endpoint) {
+
+      await axios.get(endpoint)
+        .then(res => {
+          this.photos = res.data;
+        })
+
+    },
+  },
+
+  created() {
+    this.getPhotos(endpoint);
+  }
+};
 </script>
 
 <template>
